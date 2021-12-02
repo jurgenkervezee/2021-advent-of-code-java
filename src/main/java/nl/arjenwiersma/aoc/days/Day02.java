728a641a24e04b543a00f62ac3b32d3207592d70
jurgenkervezee <jurgenbla@gmail.com>
2021-12-02 09:37:57 +0100

    added day2 class

---
+++ src/main/java/nl/arjenwiersma/aoc/days/Day02.java
@@ -0,0 +1,17 @@
+package nl.arjenwiersma.aoc.days;
+
+import nl.arjenwiersma.aoc.common.Day;
+
+import java.util.List;
+
+public class Day02 implements Day<Integer> {
+    @Override
+    public Integer part1(List<String> input) {
+        return null;
+    }
+
+    @Override
+    public Integer part2(List<String> input) {
+        return null;
+    }
+}
