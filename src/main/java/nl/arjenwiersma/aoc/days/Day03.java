171ebb8038c6c4da7a9d46cb2e8f22f4efed499c
pnl0966w <Avatar1976!>
2021-12-03 10:48:45 +0100

    day 3 part 1

--- src/main/java/nl/arjenwiersma/aoc/days/Day03.java
+++ src/main/java/nl/arjenwiersma/aoc/days/Day03.java
@@ -7,7 +7,37 @@ import java.util.List;
 public class Day03 implements Day<Integer> {
     @Override
     public Integer part1(List<String> input) {
-        return null;
+
+        StringBuilder gammaRate = new StringBuilder();
+        StringBuilder epsilonRate = new StringBuilder();
+
+        for (int i=0; i < 12 ; i++){
+            int dominant = calculateDominantZeroOrOne(input, i);
+            int sub = Math.abs(dominant - 1);
+            gammaRate.append(dominant);
+            epsilonRate.append(sub);
+        }
+        int decimalGammaRate = Integer.parseInt(String.valueOf(gammaRate), 2);
+        int decimalEpsilonRate = Integer.parseInt(String.valueOf(epsilonRate), 2);
+
+        return decimalGammaRate*decimalEpsilonRate;
+    }
+
+    public int calculateDominantZeroOrOne(List<String> input, int charIndex) {
+        int amountOfZero = 0;
+
+        for (String i : input) {
+            char chr = i.charAt(charIndex);
+
+            String str = String.valueOf(chr);
+            if (str.equals("0")) {
+                amountOfZero++;
+            }
+        }
+
+        if(amountOfZero>(input.size()-amountOfZero)){
+            return 0;
+        }else return 1;
     }
 
     @Override
